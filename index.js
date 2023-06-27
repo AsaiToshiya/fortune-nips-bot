a6d7996fd9a9f7ff319f9df3faf6aa80a560d5ff
Asai Toshiya <to.asai.60@gmail.com>
2023-06-27 19:49:47 +0900

    コメントを追加する

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ dotenv.config({ path: ".env.local", override: true });
 import { SimplePool, nip19, getPublicKey, finishEvent } from "nostr-tools";
 import "websocket-polyfill";
 
+// コンテキストを生成する
 const createContent = (post) => {
   const hash = generateHash(
     post.pubkey,
@@ -19,26 +20,33 @@ const createContent = (post) => {
 ${nip[0]}
 https://github.com/nostr-protocol/nips/blob/master/${nip[1]}`;
 };
+// タグの配列を生成する
 const createTags = (post) => {
+  // 投稿者を除外する
   const pTags = post.tags.filter(
     (tag) => tag[0] == "p" && tag[1] != post.pubkey
   );
   return [...pTags, ["e", post.id, "", "root"], ["p", post.pubkey]];
 };
+// ハッシュを数値に変換する
 const convertToNumber = (hash, range) => {
   const decimal = parseInt(hash, 16);
   return decimal % (range + 1);
 };
+// ハッシュを生成する
 const generateHash = (value1, value2) => {
   const hash = crypto.createHash("sha256");
   hash.update(value1.toString());
   hash.update(value2.toString());
   return hash.digest("hex");
 };
+// 現在の UNIX 時間を返す
 const unixTimeNow = () => Math.floor(Date.now() / 1000);
+// Date オブジェクトから時刻を取り除く
 const withoutTime = (date) =>
   new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
+// NIPs、リレー、秘密鍵、公開鍵
 const nips = fs
   .readFileSync("nips.csv")
   .toString()
@@ -48,12 +56,15 @@ const relays = JSON.parse(process.env.RELAYS.replace(/'/g, '"'));
 const sk = nip19.decode(process.env.NSEC).data;
 const pk = getPublicKey(sk);
 
+// 投稿をフェッチする範囲
 const date = parseInt(fs.readFileSync("date.txt"));
 const now = unixTimeNow();
 
+// 実行日時を更新する
 fs.writeFileSync("date.txt", now.toString());
 
 const pool = new SimplePool();
+// 投稿
 const posts = (
   await pool.list(relays, [
     {
@@ -63,7 +74,8 @@ const posts = (
       until: now,
     },
   ])
-).filter((post) => post.tags.every((tag) => tag[0] != "e"));
+).filter((post) => post.tags.every((tag) => tag[0] != "e")); // 返信に返信しない
+// 返信
 const replies = posts.map((post) =>
   finishEvent(
     {
@@ -75,6 +87,7 @@ const replies = posts.map((post) =>
     sk
   )
 );
+// 返信を発行する
 await Promise.all(
   replies.map(
     (reply) =>
@@ -85,6 +98,7 @@ await Promise.all(
       })
   )
 );
+// 後処理を実行する
 pool.close(relays);
 
 process.exit();
