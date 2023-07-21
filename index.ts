c4a72307d792570401d77c783432f600b2ae6de4
Asai Toshiya <to.asai.60@gmail.com>
2023-07-22 01:43:48 +0900

    最低限で変換する

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ import { SimplePool, nip19, getPublicKey, finishEvent } from "nostr-tools";
 import "websocket-polyfill";
 
 // コンテキストを生成する
-const createContent = (post) => {
+const createContent = (post: any) => {
   const hash = generateHash(
     post.pubkey,
     withoutTime(new Date(post.created_at * 1000)),
@@ -23,22 +23,22 @@ https://github.com/nostr-protocol/nips/blob/master/${nip.filename}`;
 };
 
 // タグの配列を生成する
-const createTags = (post) => {
+const createTags = (post: any) => {
   // 投稿者を除外する
   const pTags = post.tags.filter(
-    (tag) => tag[0] == "p" && tag[1] != post.pubkey
+    (tag: any) => tag[0] == "p" && tag[1] != post.pubkey
   );
   return [...pTags, ["e", post.id, "", "root"], ["p", post.pubkey]];
 };
 
 // ハッシュを数値に変換する
-const convertToNumber = (hash, range) => {
+const convertToNumber = (hash: any, range: any) => {
   const decimal = parseInt(hash, 16);
   return decimal % (range + 1);
 };
 
 // ハッシュを生成する
-const generateHash = (value1, value2, value3) => {
+const generateHash = (value1: any, value2: any, value3: any) => {
   const hash = crypto.createHash("sha256");
   hash.update(value1.toString());
   hash.update(value2.toString());
@@ -50,7 +50,7 @@ const generateHash = (value1, value2, value3) => {
 const unixTimeNow = () => Math.floor(Date.now() / 1000);
 
 // Date オブジェクトから時刻を取り除く
-const withoutTime = (date) =>
+const withoutTime = (date: any) =>
   new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
 // NIPs、リレー、秘密鍵、公開鍵
@@ -63,8 +63,8 @@ const nips = fs
     title: data[0],
     filename: data[1],
   }));
-const relays = JSON.parse(process.env.RELAYS.replace(/'/g, '"'));
-const sk = nip19.decode(process.env.NSEC).data;
+const relays = JSON.parse(process.env.RELAYS!.replace(/'/g, '"'));
+const sk = nip19.decode(process.env.NSEC!).data as string;
 const pk = getPublicKey(sk);
 const npub = nip19.npubEncode(pk);
 
@@ -86,21 +86,20 @@ const posts = await pool.list(relays, [
 
 // 返信済みの投稿の ID
 const repliedPostIds = posts
-  .filter((post) => post.pubkey == pk) // ボットか
-  .map((post) => post.tags.find((tag) => tag[0] == "e")[1]);
+  .filter((post: any) => post.pubkey == pk) // ボットか
+  .map((post: any) => post.tags.find((tag: any) => tag[0] == "e")[1]);
 
 // メンション
 // prettier-ignore
 const mentions = posts.filter(
-  (post) =>
-    post.pubkey != pk &&                       // ボットではないか
-    !repliedPostIds.includes(post.id) &&       // 返信済みではないか
-    post.tags.every((tag) => tag[0] != "e") && // 返信に返信しない
-    post.content.includes(`nostr:${npub}`)     // content にボットの npub を含むか
+  (post: any) => post.pubkey != pk &&                       // ボットではないか
+  !repliedPostIds.includes(post.id) &&       // 返信済みではないか
+  post.tags.every((tag: any) => tag[0] != "e") && // 返信に返信しない
+  post.content.includes(`nostr:${npub}`)     // content にボットの npub を含むか
 );
 
 // 返信
-const replies = mentions.map((post) =>
+const replies = mentions.map((post: any) =>
   finishEvent(
     {
       kind: 1,
@@ -115,7 +114,7 @@ const replies = mentions.map((post) =>
 // 返信を発行する
 await Promise.all(
   replies.map(
-    (reply) =>
+    (reply: any) =>
       new Promise((resolve, reject) => {
         const pub = pool.publish(relays, reply);
         pub.on("ok", resolve);
